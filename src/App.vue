49da5536720c3aec80d0fddf7db5120dd231f829
SantosPereira <pedrohenriquelemam@gmail.com>
2021-09-18 18:54:34 -0300

    Criação do projeto Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Cabecalho msg="Stella Venator"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cabecalho from './components/Cabecalho.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cabecalho
   }
 }
 </script>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cabecalho">
+    <h1>{{ msg }}</h1>
+    <p>Monte seu telescópio e explore o universo.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cabecalho',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
