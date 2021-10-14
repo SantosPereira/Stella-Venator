e7d4b459187ae8db6506f8cb5ff581244ab346f2
SantosPereira <pedrohenriquelemam@gmail.com>
2021-10-14 16:38:20 -0300

    Pega valores do formulário

--- src/components/ParametrosDoTele.vue
+++ src/components/ParametrosDoTele.vue
@@ -2,39 +2,71 @@
   <form>
     <div class="capsula">
       <label>Qual tipo de objetos quer observar</label>
-      <select>
-        <option></option>
-        <option>Objeto terrestres</option>
-        <option>Apenas a Lua</option>
-        <option>
+      <select id="tipoObj">
+        <option value="0"></option>
+        <option value="1">Objeto terrestres</option>
+        <option value="2">Apenas a Lua</option>
+        <option value="3">
           Lua em alta resolução, aglomerados de estrelas, objetos de alto brilho
         </option>
-        <option>Planetas do sistema solar</option>
-        <option>Nebulosas e galáxias</option>
+        <option value="4">Planetas do sistema solar</option>
+        <option value="5">Nebulosas e galáxias</option>
       </select>
     </div>
 
     <div class="capsula">
       <label>Diâmetro da lente Objetiva</label>
-      <input type="text" placeholder="70mm" />
+      <input type="text" placeholder="70mm" id="diam"/>
     </div>
 
     <div class="capsula">
       <label>Distância focal da Objetiva</label>
-      <input type="text" placeholder="500mm" />
+      <input type="text" placeholder="500mm" id="disFocObj"/>
     </div>
 
     <div class="capsula">
       <label>Distância focal da lente Ocular</label>
-      <input type="text" placeholder="20mm" />
+      <input type="text" placeholder="16mm" id="disFocOcu"/>
     </div>
+
+    <button v-on:click="pegaValor">Calcular</button>
   </form>
 </template>
 
 <script>
 export default {
-  name: "ParametrosDoTele",
-};
+    name: 'Form',
+    data: function() {
+    return {
+        valor: 16,
+        outroValor: 32
+    }
+    },
+    methods: {
+        pegaValor: function () {
+            // var tipoObj = document.getElementById("tipoObj").value
+            var diam = document.getElementById("diam").value
+            var disFocObj = document.getElementById("disFocObj").value
+            var disFocOcu = document.getElementById("disFocOcu").value
+
+            diam = parseInt(diam.toLowerCase().replace('mm','').trim())
+            disFocObj = parseInt(disFocObj.toLowerCase().replace('mm','').trim())
+            disFocOcu = parseInt(disFocOcu.toLowerCase().replace('mm','').trim())
+
+            var F = (disFocObj+disFocOcu)/diam
+            var amp = disFocObj/disFocOcu
+            var ampMax = disFocObj/disFocOcu
+
+            console.log("\n\n\n")
+            console.log("Razão de focal: F/",F)
+            console.log("Ampliação: ",amp)
+            console.log("Ampliação máxima para o arranjo óptico: ",ampMax) // diam/25.4 é a converção para polegada
+
+            return [diam, disFocObj, disFocOcu, F, amp, ampMax]
+        }
+    }
+}
+
 </script>
 
 <style scoped>
